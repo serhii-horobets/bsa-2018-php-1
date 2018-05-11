d5875d3a736eade2557480a85e8fc91109f5c19e
serhii-horobets <serhii.horobets@gmail.com>
2018-05-11 22:05:06 +0300

    task 1

---
+++ src/Task1/Ethereum.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Cryptocurrency\Task1;
+
+class Ethereum extends AbstractCoins
+{
+    public static $dailyPrice;
+    const LOGO = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png';
+}
