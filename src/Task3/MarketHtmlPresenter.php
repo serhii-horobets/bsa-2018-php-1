f04397e2899fc356e0deedc5f6c1b70a3f1ddcb3
serhii-horobets <serhii.horobets@gmail.com>
2018-05-11 22:13:13 +0300

    task 3

--- src/Task3/MarketHtmlPresenter.php
+++ src/Task3/MarketHtmlPresenter.php
@@ -8,6 +8,27 @@ class MarketHtmlPresenter
 {
     public function present(CoinMarket $market): string
     {
-        // @todo
+        $maxPrice = $market->maxPrice();
+
+        $presentation = '<div class="container">
+                            <table border="1" cellspacing="0" cellpadding="10">
+                                <caption><b>Coin Market</b>, <b style="color:#ff989d">max price</b></caption> 
+                                <thead>
+                                    <tr>
+                                        <th>Logo</th>
+                                        <th>Name: Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>';        
+        foreach ($market->getCurrencies() as $currency) {
+            if ($currency->getDailyPrice() == $maxPrice) {
+                $colorRow = ' bgcolor="#ff989d"'; //fill row with max price
+            } else {
+                $colorRow = '';
+            }
+            $presentation .= '<tr' . $colorRow . '><td><img src="' . $currency->getLogoUrl() . '"></td><td>' . $currency->getName() . ': ' . $currency->getDailyPrice() . '</td></tr>';
+        }
+        $presentation .= '</tbody></table></div>';
+        return $presentation;
     }
 }
