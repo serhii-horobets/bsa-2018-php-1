43c8d0c5d72ee310878eb85a362fd2044014ee06
serhii-horobets <serhii.horobets@gmail.com>
2018-05-14 16:02:08 +0300

    fix $dailyPrice to protected

--- src/Task1/Ethereum.php
+++ src/Task1/Ethereum.php
@@ -4,6 +4,6 @@ namespace Cryptocurrency\Task1;
 
 class Ethereum extends AbstractCoins
 {
-    public static $dailyPrice;
+    protected static $dailyPrice;
     const LOGO = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png';
 }
